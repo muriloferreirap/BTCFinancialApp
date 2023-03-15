4d276dcbb560e4b45564ea9e4a15e95a91d4bac5
muriloferreirap <muriloferreirap1956@gmail.com>
2023-03-15 20:51:33 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BitcoinTicker from "./components/BitcoinTicker";
+
+const App = () => {
+  return (
+    <>
+      <BitcoinTicker />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/BitcoinTicker.tsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useInterval } from "use-interval";
+import "./BitcoinTicker.css";
+
+const BitcoinTicker = () => {
+  const [buy, setBuy] = useState(0);
+
+  const fetchTicker = async () => {
+    const response = await axios.get(
+      "https://api.coingecko.com/api/v3/exchange_rates/"
+    );
+    setBuy(response.data.rates.brl.value);
+  };
+
+  useEffect(() => {
+    fetchTicker();
+  }, []);
+
+  useInterval(() => {
+    console.log(buy);
+    fetchTicker();
+  }, 5000);
+
+  const formattedValue = buy.toLocaleString("pt-br", {
+    style: "currency",
+    currency: "BRL",
+  });
+
+  return (
+    <div className="container">
+      <h1 className="title">Preço do Bitcoin</h1>
+      <p className="buy-price">{formattedValue}</p>
+    </div>
+  );
+};
+
+export default BitcoinTicker;
